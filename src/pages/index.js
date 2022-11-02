0b564b927dc2e532ebe9001b6a050ec361479ae9
fintech-enigma <116291737+fintech-enigma@users.noreply.github.com>
2022-11-02 08:45:27 +0100

    Update index.js
    Bruk siteconfig sine variabler for hovedoverskrift og "slagord"..

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,8 +12,8 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{"Fintech Enigma"}</h1>
-        <p className="hero__subtitle">{"Fintech er kult!"}</p>
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
