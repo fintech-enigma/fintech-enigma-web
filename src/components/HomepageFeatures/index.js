5fb53c8dd67f54fb9d9fe526c448d8343f831d6a
Emilie Pedersen <emiliepedersen@Emilies-MacBook-Air-2.local>
2022-11-24 16:30:47 +0100

    Begynt paa Featureliste

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -2,36 +2,21 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
 
-const FeatureList = [
+const FeatureList1 = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
+    title: 'PORTEFØLJE',
+    //Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default
+  }
+];
+
+const FeatureList2 = [
+  {
+    title: 'FOR STUDENTER',
+    //Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
+    title: 'FOR BEDRIFTER',
+    //Svg: require('@site/static/img/undraw_docusaurus_react.svg').default
   },
 ];
 
@@ -39,7 +24,7 @@ function Feature({Svg, title, description}) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        {/* <Svg claskksName={styles.featureSvg} role="img" /> */}
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
@@ -54,7 +39,10 @@ export default function HomepageFeatures() {
     <section className={styles.features}>
       <div className="container">
         <div className="row">
-          {FeatureList.map((props, idx) => (
+          {FeatureList1.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+          {FeatureList2.map((props, idx) => (
             <Feature key={idx} {...props} />
           ))}
         </div>
